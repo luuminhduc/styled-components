a436f630fd50cb9ec03ab76328f5eb7322438b38
unknown <luuminhduc00@gmail.com>
2021-11-26 12:58:33 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "./components/styled/Container.styled";
+
+const App = () => {
+  return (
+    <div>
+      <Container>
+        <p>Hello</p>
+        <h1>LSDOSDOP</h1>
+      </Container>
+      <h1>sdsadqw</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/styled/Container.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 1000px;
+  background-color: red;
+  color: blue;
+  h1 {
+    color: yellow;
+  }
+`;
