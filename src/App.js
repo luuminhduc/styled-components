bcdab615fdfa50965489e6c2426ade4be903c5cf
unknown <luuminhduc00@gmail.com>
2021-11-26 15:09:45 +0700

    set up bacsic style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Container } from "./components/styled/Container.styled";
+import GlobalStyle from "./components/styled/Global";
+import { ThemeProvider } from "styled-components";
+import theme from "./components/styled/theme";
 
 const App = () => {
   return (
-    <div>
-      <Container>
-        <p>Hello</p>
-        <h1>LSDOSDOP</h1>
-      </Container>
-      <h1>sdsadqw</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Container></Container>
+    </ThemeProvider>
   );
 };
 
---
+++ src/components/styled/theme.js
@@ -0,0 +1,8 @@
+const theme = {
+  colors: {
+    light: "#F3F3FB",
+    dark: "#2F334D",
+    gray: "#8C8C9C",
+  },
+};
+export default theme;
